c4d9e1d3158133f3979711273218f08a57d89d4e
liliana Quinley <lilicuellar99@gmail.com>
2021-04-14 21:44:13 -0600

    heroku second try

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send({Hello: "Heroku application API" })
+    res.send({Hello: "Heroku  API" })
 });
 
 
